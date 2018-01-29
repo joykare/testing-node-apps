590b483b6bb326eac79200796d755667a02e29f0
joykare <warugujoy@gmail.com>
2018-01-29 18:23:50 +0300

    (feat): use stubs for testing

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -1,6 +1,9 @@
 module.exports = {
   // A func that takes in two parameters `req` and `res` [request, response]
   getIndexPage: (req, res) => {
-    res.send("Hey");
+    if (req.user.isLoggedIn()) {
+      return res.send("Hey");
+    }
+    res.send("Ooops. You need to log in to access this page");
   }
 }
--- tests/controllers/app.controller.test.js
+++ tests/controllers/app.controller.test.js
@@ -4,20 +4,68 @@ const expect = chai.expect;
 const sinon = require("sinon");
 const indexPage = require("../../controllers/app.controller.js");
 
-const user = {
-  addUser: (name) => {
-    this.name = name;
-  }
-}
-describe("User", function() {
-  describe("addUser", function() {
-    it("should set name", function() {
-      sinon.spy(user, "addUser");
-      user.addUser('John Doe');
+describe("AppController", function()  {
+  describe("getIndexPage", function() {
+    it("should send hey", function() {
+      // instantiate a user object with an empty isLoggedIn function
+      let user = {
+        isLoggedIn: function(){}
+      }
 
-      // lets log `addUser` and see what we get
-      console.log(user.addUser);
-      expect(user.addUser.calledOnce).to.be.true;
-    })
-  })
-})
+      // Stub isLoggedIn function and make it return true always
+      const isLoggedInStub = sinon.stub(user, "isLoggedIn").returns(true);
+
+      // pas user into the req object
+      let req = {
+        user: user
+      }
+
+      // Have `res` have a send key with a function value coz we use `res.send()` in our func
+      let res = {
+        // replace empty function with a spy
+        send: sinon.spy()
+      }
+
+      indexPage.getIndexPage(req, res);
+      // let's see what we get on res.send
+      // console.log(res.send);
+      // `res.send` called once
+      expect(res.send.calledOnce).to.be.true;
+      expect(res.send.firstCall.args[0]).to.equal("Hey");
+
+      // assert that the stub is logged in at least once
+      expect(isLoggedInStub.calledOnce).to.be.true;
+    });
+
+    it("should send something else when user is NOT logged in", function() {
+      // instantiate a user object with an empty isLoggedIn function
+      let user = {
+        isLoggedIn: function(){}
+      }
+
+      // Stub isLoggedIn function and make it return false always
+      const isLoggedInStub = sinon.stub(user, "isLoggedIn").returns(false);
+
+      // pass user into the req object
+      let req = {
+        user: user
+      }
+
+      // Have `res` have a send key with a function value coz we use `res.send()` in our func
+      let res = {
+        // replace empty function with a spy
+        send: sinon.spy()
+      }
+
+      indexPage.getIndexPage(req, res);
+      // let's see what we get on res.send
+      // console.log(res.send);
+      // `res.send` called once
+      expect(res.send.calledOnce).to.be.true;
+      expect(res.send.firstCall.args[0]).to.equal("Ooops. You need to log in to access this page");
+
+      // assert that the stub is logged in at least once
+      expect(isLoggedInStub.calledOnce).to.be.true;
+    });
+  });
+});
