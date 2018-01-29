12f592f3fe7a433d0347b83d5b274b51b9aac1e7
joykare <warugujoy@gmail.com>
2018-01-29 21:05:17 +0300

    (feat): test async functions using promises

--- tests/ayncFunc.test.js
+++ tests/ayncFunc.test.js
@@ -1,29 +1,24 @@
 const chai = require("chai");
 const expect = chai.expect;
+chai.use(require("chai-as-promised"));
 
 // This is just an async func that takes in a bool
-// and calls a callback that returns a some message
-// depending on the bool value
+// and that returns a promise
 function someMadeUpAyncFunc(boolValue, cb) {
-  setTimeout(function() {
-    cb(boolValue ? "You get a sweet :)" : "You get nothing!!")
-  }, 0);
+  return new Promise(function(resolve){
+    setTimeout(function() {
+      resolve(boolValue ? "You get a sweet :)" : "You get nothing!!")
+    }, 0);
+  })
 }
 
 // Added the `only` tag to have only this set of tests to run
 describe.only("AsyncTest", function()  {
-  it("should return `You get a sweet :)` if `true` is passed in", function(done) {
-    someMadeUpAyncFunc(true, function(sweetCheck){
-      expect(sweetCheck).to.equal("You get a sweet :)");
-      done();
-    });
+  it("should return `You get a sweet :)` if `true` is passed in", function() {
+    return expect(someMadeUpAyncFunc(true)).to.eventually.equal("You get a sweet :)");
   });
 
-  it("should return `You get nothing!!` if `false` is passed in", function(done) {
-    someMadeUpAyncFunc(false, function(sweetCheck){
-      // Let's fail it on purpose just to see what happens
-      expect(sweetCheck).to.equal("You get nothing!!");
-      done();
-    });
+  it("should return `You get nothing!!` if `false` is passed in", function() {
+    return expect(someMadeUpAyncFunc(false)).to.eventually.equal("You get nothing!!");
   });
 });
